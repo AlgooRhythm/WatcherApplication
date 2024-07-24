271a0b35523a0fd0427a8e3044f351f6053127ee
Muhaimin Khamsan Lenovo <muhaiminkhamsan@gmail.com>
2024-07-24 21:21:33 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace WatcherApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher2 = new System.IO.FileSystemWatcher();
+            this.textBoxLog = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputFolderPath = new System.Windows.Forms.TextBox();
+            this.Watch = new System.Windows.Forms.Button();
+            this.ClearLog = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create a sample .json files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.CreateASampleFiles_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // fileSystemWatcher2
+            // 
+            this.fileSystemWatcher2.EnableRaisingEvents = true;
+            this.fileSystemWatcher2.SynchronizingObject = this;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(173, 76);
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(571, 299);
+            this.textBoxLog.TabIndex = 2;
+            this.textBoxLog.Text = "";
+            this.textBoxLog.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folder Path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // InputFolderPath
+            // 
+            this.InputFolderPath.Location = new System.Drawing.Point(173, 30);
+            this.InputFolderPath.Name = "InputFolderPath";
+            this.InputFolderPath.Size = new System.Drawing.Size(483, 20);
+            this.InputFolderPath.TabIndex = 4;
+            this.InputFolderPath.Text = "C:\\TestWatcherApps\\";
+            this.InputFolderPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Watch
+            // 
+            this.Watch.Location = new System.Drawing.Point(672, 30);
+            this.Watch.Name = "Watch";
+            this.Watch.Size = new System.Drawing.Size(72, 22);
+            this.Watch.TabIndex = 5;
+            this.Watch.Text = "Watch";
+            this.Watch.UseVisualStyleBackColor = true;
+            this.Watch.Click += new System.EventHandler(this.Watch_Click);
+            // 
+            // ClearLog
+            // 
+            this.ClearLog.Location = new System.Drawing.Point(31, 332);
+            this.ClearLog.Name = "ClearLog";
+            this.ClearLog.Size = new System.Drawing.Size(97, 43);
+            this.ClearLog.TabIndex = 6;
+            this.ClearLog.Text = "Clear Log";
+            this.ClearLog.UseVisualStyleBackColor = true;
+            this.ClearLog.Click += new System.EventHandler(this.ClearLog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClearLog);
+            this.Controls.Add(this.Watch);
+            this.Controls.Add(this.InputFolderPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.RichTextBox textBoxLog;
+        private System.IO.FileSystemWatcher fileSystemWatcher2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputFolderPath;
+        private System.Windows.Forms.Button Watch;
+        private System.Windows.Forms.Button ClearLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System;
+using Newtonsoft.Json;
+
+namespace WatcherApplication
+{
+    public partial class Form1 : Form
+    {
+        private FileSystemWatcher fileSystemWatcher;
+        private string oldInputFolderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFileSystemWatcher();
+        }
+
+        private void InitializeFileSystemWatcher() { }
+
+        private void WatchActivity()
+        {
+            string newInputFolderPath = InputFolderPath.Text;
+
+            if (fileSystemWatcher == null || oldInputFolderPath != newInputFolderPath)
+            {
+                string InitialWatchMessage = $"Initial Watch folder {InputFolderPath.Text}";
+                LogMessage(InitialWatchMessage);
+
+                fileSystemWatcher = new FileSystemWatcher
+                {
+                    NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
+                    Filter = "*.*" // Watch all files
+                };
+
+                oldInputFolderPath = newInputFolderPath;
+            }
+            else
+            {
+                string ReWatchMessage = $"Re-Watch Folder {InputFolderPath.Text}";
+                LogMessage(ReWatchMessage);
+            }
+
+            // Unsubscribe from the previous event handler if it was set
+            fileSystemWatcher.Created -= OnNewFileDetected;
+            fileSystemWatcher.Renamed -= OnFileRenamed;
+            fileSystemWatcher.Deleted -= OnFileDeleted;
+
+            // Update the path to monitor
+            fileSystemWatcher.Path = InputFolderPath.Text;
+
+            // Subscribe to the Created event
+            fileSystemWatcher.Created += OnNewFileDetected;
+            fileSystemWatcher.Renamed += OnFileRenamed;
+            fileSystemWatcher.Deleted += OnFileDeleted;
+
+            // Start monitoring
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void Watch_Click(object sender, EventArgs e)
+        {
+            fileSystemWatcher = new FileSystemWatcher();
+
+            if (Directory.Exists(InputFolderPath.Text))
+            {
+                WatchActivity();
+            }
+            else //Create directory if not exist yet
+            {
+                Directory.CreateDirectory(InputFolderPath.Text);
+
+                string logMessage = $"New Folder created in: {InputFolderPath.Text} at {DateTime.Now}";
+                LogMessage(logMessage);
+
+                WatchActivity();
+            }
+        }
+
+        private void OnNewFileDetected(object sender, FileSystemEventArgs e)
+        {
+            if (File.Exists(e.FullPath))
+            {
+                string logMessage = $"New File detected in: {e.FullPath} at {DateTime.Now}";
+                LogMessage(logMessage);
+            }
+            else
+            {
+                string logMessage = $"Path {e.FullPath} not exist. Please specify path that exist";
+                LogMessage(logMessage);
+            }
+        }
+
+        private void OnFileRenamed(object source, RenamedEventArgs e)
+        {
+            if (File.Exists(e.FullPath))
+            {
+                string logMessage = $"Existing file rename from {e.OldFullPath} to {e.FullPath} at {DateTime.Now}";
+                LogMessage(logMessage);
+            }
+            else
+            {
+                string logMessage = $"Path {e.FullPath} not exist. Please specify path that exist";
+                LogMessage(logMessage);
+            }
+        }
+
+        private void OnFileDeleted(object sender, FileSystemEventArgs e)
+        {
+            string logMessage = $"File deleted in: {e.FullPath} at {DateTime.Now}";
+            LogMessage(logMessage);
+        }
+
+        private void LogToFile(string message)
+        {
+            string logFilePath = String.Concat(InputFolderPath.Text, $"\\LogFiles\\LOGFILE-{DateTime.Now.ToString("yyyyMMdd")}.txt");
+            string logDirectory = Path.GetDirectoryName(logFilePath);
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            // Check for file existence and attributes
+            if (File.Exists(logFilePath))
+            {
+                FileAttributes attributes = File.GetAttributes(logFilePath);
+                if ((attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                {
+                    MessageBox.Show("The log file is read-only.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            // Include the date and time before the message
+            string logEntry = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {message}";
+
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine(logEntry);
+            }
+        }
+
+        private void LogToTextBox(string message)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<string>(LogToTextBox), message);
+            }
+            else
+            {
+                string displayMessage = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {message}";
+
+                textBoxLog.AppendText(displayMessage + Environment.NewLine + Environment.NewLine);
+            }
+        }
+
+        private void LogMessage(string logMessage)
+        {
+            try
+            {
+                LogToFile(logMessage);
+                LogToTextBox(logMessage);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Access denied to the log file path: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Initialize components on form load
+        }
+
+        private void CreateASampleFiles_Click(object sender, EventArgs e)
+        {
+            CreateJsonFile();
+        }
+
+        private void CreateJsonFile()
+        {
+            try
+            {
+                string directoryPath = InputFolderPath.Text;
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+
+                    string logMessage = $"New folder created in: {directoryPath}";
+                    LogMessage(logMessage);
+                }
+
+                string filePath = Path.Combine(directoryPath, $"{DateTime.Now.ToString("yyyyMMdd")}-sample.json");
+
+                #region Sample data
+                //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                var sampleData = new
+                {
+                    CreatedDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                    Name = "Muhaimin Khamsan",
+                    Age = 27,
+                    Occupation = "Software Developer"
+                };
+                //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                #endregion
+
+                string jsonContent = JsonConvert.SerializeObject(sampleData, Formatting.Indented);
+
+                // Check if file exists
+                if (File.Exists(filePath))
+                {
+                    // Append data to the file
+                    File.AppendAllText(filePath, jsonContent + Environment.NewLine);
+
+                    string logMessage = $"Existing file append in: {filePath}";
+                    LogMessage(logMessage);
+                }
+                else
+                {
+                    // Write new data to the file
+                    File.WriteAllText(filePath, jsonContent + Environment.NewLine);
+
+                    string logMessage = $"New file created in: {filePath}";
+                    LogMessage(logMessage);
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Access denied to the JSON file path: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearLog_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Clear();
+        }
+    }
+}
